3f2fb3b02fa7ac41e8142911dc1d455d822294d1
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-06 15:04:59 +0300

    Update Main.java

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -1,15 +1,16 @@
 package com.samsung.learning;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        int a = scanner.nextInt(), b = scanner.nextInt();
+        scanner.useLocale(Locale.US);
 
-        System.out.println(a * a + b * b);
-
+        double x = scanner.nextDouble();
+        System.out.println(x >= 3 && x <= 8);
     }
 
 }
