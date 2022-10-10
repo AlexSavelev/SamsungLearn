b84ecbeed02c1a9a5c2b69541653d4d98ce8629f
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-10 22:21:59 +0300

    t4

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -7,16 +7,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        int a = scanner.nextInt();
+        int a = scanner.nextInt(), b = -1 * scanner.nextInt();
 
-        System.out.print(a + " ");
-
-        if((a / 10) % 10 == 1 || a % 10 >= 5 || a % 10 == 0) {
-            System.out.println("TORTOV");
-        } else if(a % 10 == 1) {
-            System.out.println("TORT");
+        // a|x| > b
+        if(a == 0) {
+            System.out.println(0 > b ? "any x" : "no such x");
         } else {
-            System.out.println("TORTA");
+            double p = (double)(b) / a;
+            if(a > 0) {
+                // |x| > p
+                if(p < 0) {
+                    System.out.println("any x");
+                } else {
+                    System.out.println("x<" + -1.0 * p + " or x>" + p);
+                }
+            } else {
+                // |x| < p
+                if(p <= 0) {
+                    System.out.println("no such x");
+                } else {
+                    System.out.println(-1.0 * p + "<x<" + p);
+                }
+            }
         }
 
     }
