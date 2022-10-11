018a5737954e06896351423a651c06427b5fe750
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-11 17:44:44 +0300

    t5

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -9,23 +9,19 @@ public class Main {
 
         int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
 
-        if(a > b) {
-            int t = a;
-            a = b;
-            b = t;
-        }
-        if(a > c) {
-            int t = a;
-            a = c;
-            c = t;
-        }
-        if(b > c) {
-            int t = b;
-            b = c;
-            c = t;
-        }
+        if(a > b) { int t = a; a = b; b = t; }
+        if(a > c) { int t = a; a = c; c = t; }
+        if(b > c) { int t = b; b = c; c = t; }
 
-        System.out.println(a + " " + b + " " + c);
+        if(c >= a + b) {
+            System.out.println("impossible");
+        } else if(a * a + b * b == c * c) {
+            System.out.println("right");
+        } else if(a * a + b * b > c * c) {
+            System.out.println("acute");
+        } else {
+            System.out.println("obtuse");
+        }
 
     }
 
