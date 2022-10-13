3f50d109f895a8b547396bc38acbdce4f346db32
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-13 14:58:44 +0300

    extra3

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -7,21 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        int a = scanner.nextInt(), b = scanner.nextInt();
+        int n = scanner.nextInt();
 
-        boolean setMinus = (a >= 0 || b >= 0) && (a <= 0 || b <= 0);
-
-        if(a == 0 || b == 0) {
-            System.out.println(0);
-        } else {
-            a = a > 0 ? a : -a;
-            int t = a;
-            b = b > 0 ? b : -b;
-            while(b > 1) {
-                a += t;
-                --b;
-            }
-            System.out.println((setMinus ? "-" : "") + a);
+        int i = 0;
+        int t = 2;
+        while(i < n) {
+            System.out.print(t + " ");
+            t += 2;
+            ++i;
         }
 
 
