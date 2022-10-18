de8b83df048120eddeecc3c1d6a042dafc9c57de
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 17:35:04 +0300

    INIT_0

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -5,20 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-
-<<<<<<< Updated upstream
-        int r = 0;
-        int a = scanner.nextInt();
-        while(a / 10 != 0 && a / 100 == 0) {
-            r += a % 10;
-            r += (a / 10) % 10;
-            a = scanner.nextInt();
-        }
-=======
-
->>>>>>> Stashed changes
-
-        System.out.println(r);
+        
 
     }
 
