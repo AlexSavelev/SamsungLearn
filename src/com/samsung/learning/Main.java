a39185456bee2e6d413eb17e9247ee5254ed8383
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 18:43:11 +0300

    extra2_t1

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -8,12 +8,25 @@ public class Main {
         Scanner scanner = new Scanner (System.in);
 
         int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; ++i)
+            arr[i] = i + 1;
+        int a = scanner.nextInt() - 1, b = scanner.nextInt() - 1, c = scanner.nextInt() - 1, d = scanner.nextInt() - 1;
 
-        int t = 1;
-        for(int i = 1; i <= n; ++i) {
-            t *= i;
+        while(a < b) {
+            int t = arr[a];
+            arr[a++] = arr[b];
+            arr[b--] = t;
         }
-        System.out.println(t);
+
+        while(c < d) {
+            int t = arr[c];
+            arr[c++] = arr[d];
+            arr[d--] = t;
+        }
+
+        for(int i: arr)
+            System.out.print(i + " ");
 
     }
 
