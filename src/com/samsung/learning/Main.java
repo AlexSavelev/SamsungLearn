5f51a629ff914d5c09c5d5e572aefaecf68222c2
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 17:49:11 +0300

    t1

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -7,9 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        int n = scanner.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; ++i)
+            a[i] = scanner.nextInt();
 
-
-        System.out.println(8);
+        int find = scanner.nextInt();
+        boolean flag = true;
+        for(int i = 0; i < n; ++i) {
+            if(a[i] == find) {
+                flag = false;
+                System.out.println(i + 1);
+                break;
+            }
+        }
+        if(flag)
+            System.out.println("NO");
 
     }
 
