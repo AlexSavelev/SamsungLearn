bf20843fb8d42c24dbd4243f8d0d9dad7315f457
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 18:48:15 +0300

    extra2_t5

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -13,8 +13,8 @@ public class Main {
             arr[i] = scanner.nextInt();
 
         int r = 0;
-        for(int i = 0; i < n; ++i) {
-            if(arr[i] > 0)
+        for(int i = 1; i < n; ++i) {
+            if(arr[i] > arr[i - 1])
                 ++r;
         }
         System.out.println(r);
