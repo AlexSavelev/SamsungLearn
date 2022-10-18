0e1d34f477e48a9d301c222dec453fc351dd3fe4
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 18:17:24 +0300

    t7

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -12,19 +12,23 @@ public class Main {
         for(int i = 0; i < n; ++i)
             a[i] = scanner.nextInt();
 
-        int record = 0, recordNum = 0;
-        for(int i: a) {
-            int t = 0;
-            for(int j: a) {
-                if(j == i)
-                    ++t;
-            }
-            if(t > recordNum) {
-                record = i;
-                recordNum = t;
-            }
+        int index = 0;
+        for(; index < n; ++index) {
+            if(a[index] > 0) break;
         }
-        System.out.println(record);
+
+        for(int i = index; i < n; ++i) {
+            if(a[i] >= 0)
+                continue;
+            int t = a[i];
+            for(int j = i - 1; j >= index; --j) {
+                a[j + 1] = a[j];
+            }
+            a[index++] = t;
+        }
+
+        for(int i: a)
+            System.out.print(i + " ");
 
     }
 
