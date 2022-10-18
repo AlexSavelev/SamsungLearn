0c6f0a72deca87f7b4e42b47c95d16ff57f4e766
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-18 17:55:23 +0300

    t3

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -7,18 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        int r = 0;
+        int r = 0, m = 0;
         boolean flag = true;
 
         int n = scanner.nextInt();
         for(int i = 0; i < n; ++i) {
             int t = scanner.nextInt();
-            if(t % 2 == 0) {
+            if(t % 2 == 1 || t % 2 == -1) {
                 flag = false;
                 r += t;
+                ++m;
             }
         }
-        System.out.println((flag ? "NO" : r));
+        System.out.println((flag ? "NO" : String.format("%.2f", (float)r / m)));
 
     }
 
