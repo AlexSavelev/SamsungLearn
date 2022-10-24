b41548844f3d20b46597d20d53c8addd618361a7
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-24 21:34:04 +0300

    t3

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -3,13 +3,19 @@ package com.samsung.learning;
 import java.util.Scanner;
 
 public class Main {
-    public static int sumOfSeven(int a, int b) {
-        while(a % 7 != 0 || (a / 10) % 10 == 0)
-            ++a;
+    public static int sumOfDigits(int n) {
         int t = 0;
-        while(a <= b && a / 100 == 0) {
-            t += (a % 10) + ((a / 10) % 10);
-            a += 7;
+        while(n != 0) {
+            t += n % 10;
+            n /= 10;
+        }
+        return t;
+    }
+    public static int sumOfThirteen(int a) {
+        int t = 0;
+        for(int i = 0; i < a; ++i) {
+            if(sumOfDigits(i) % 13 == 0)
+                ++t;
         }
         return t;
     }
@@ -17,9 +23,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        int a = scanner.nextInt(), b = scanner.nextInt();
+        int a = scanner.nextInt();
 
-        System.out.println(sumOfSeven(a, b));
+        System.out.println(sumOfThirteen(a));
 
     }
 
