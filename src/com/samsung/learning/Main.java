f50efb95c485d0e646e0c32e75317e80aa4a01e1
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-24 22:00:48 +0300

    t2_extra

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -3,14 +3,12 @@ package com.samsung.learning;
 import java.util.Scanner;
 
 public class Main {
-    public static double segmentLength(long x1, long y1, long x2, long y2) {
-        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    public static int minOf2(int a, int b) {
+        return (a < b) ? a : b;
     }
-
-    public static double trianglePerimetr(long x1, long y1, long x2, long y2, long x3, long y3) {
-        return segmentLength(x1, y1, x2, y2) + segmentLength(x2, y2, x3, y3) + segmentLength(x1, y1, x3, y3);
+    public static int min(int a, int b, int c, int d) {
+        return minOf2(minOf2(a, b), minOf2(c, d));
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
