0ae10e16199eac8ba31099f4d126b9e676f51860
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-24 22:38:18 +0300

    t5

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -3,22 +3,39 @@ package com.samsung.learning;
 import java.util.Scanner;
 
 public class Main {
+    public static int digitCount(int a) {
+        int t = (a == 0 ? 1 : 0);
+        while(a != 0) {
+            ++t;
+            a /= 10;
+        }
+        return t;
+    }
     public static void printMatrix(int a[][]) {
         for(int t[]: a) {
-            for(int i: t)
-                System.out.print(i + " ");
+            for(int i: t) {
+                int k = 3 - digitCount(i);
+                for(int j = 0; j < k; ++j)
+                    System.out.print(" ");
+                System.out.print(i);
+            }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        int [][] a;
-
         Scanner scanner = new Scanner (System.in);
         int n = scanner.nextInt(), m = scanner.nextInt();
-        a = new int[n][m];
+        int a[][] = new int[n][m];
 
-        for(int i = 0; i < n * m; ++i) {
-            a[i / m][i % m] = (i / m) * (i % m);
+        int t = 0;
+        for(int i = 0; i < n; ++i) {
+            if(i % 2 == 0) {
+                for (int j = 0; j < m; ++j)
+                    a[i][j] = t++;
+            } else {
+                for (int j = m - 1; j >= 0; --j)
+                    a[i][j] = t++;
+            }
         }
 
         printMatrix(a);
