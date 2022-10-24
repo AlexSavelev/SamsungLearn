64fc0411afc8107025080c6708e71ec36dfb67bd
AlexSavelev <65019464+AlexSavelev@users.noreply.github.com>
2022-10-24 21:31:31 +0300

    t2_update

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static int sumOfSeven(int a, int b) {
-        while(a % 7 != 0)
+        while(a % 7 != 0 || (a / 10) % 10 == 0)
             ++a;
         int t = 0;
-        while(a <= b && a / 100 == 0 && (a / 10) % 10 != 0) {
+        while(a <= b && a / 100 == 0) {
             t += (a % 10) + ((a / 10) % 10);
             a += 7;
         }
